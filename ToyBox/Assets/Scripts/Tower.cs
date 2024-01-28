4ff1fe4f612c630e26753976912ad852c3008a90
Ayratos <129599339+Ayratos@users.noreply.github.com>
2024-01-28 08:35:46 +0300

    Upstairs

--- ToyBox/Assets/Scripts/Tower.cs
+++ ToyBox/Assets/Scripts/Tower.cs
@@ -5,18 +5,29 @@ using UnityEngine;
 public class Tower : MonoBehaviour
 {
     bool towered;
+    
     public GameObject text;
+    [Header("Keybinds")]
+    public KeyCode Key = KeyCode.Space;
+    public Outline outline;
     //public GameObject[] soldiers;
+    public Transform TowerPos;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Tower"))
         {
             towered = true;
+            outline.enabled = true;
+            if (Input.GetKeyDown(Key))
+            {
+                transform.position = TowerPos.position;
+            }
 
         }
         else
         {
             towered = false;
+            outline.enabled = false;
         }
     }
     private void OnTriggerExit(Collider other)
@@ -24,6 +35,7 @@ public class Tower : MonoBehaviour
         if (other.gameObject.CompareTag("Tower"))
         {
             towered = false;
+            outline.enabled = false;
         }
     }
     private void Update()
